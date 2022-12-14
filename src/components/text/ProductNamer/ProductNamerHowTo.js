222e2387f47ea623f2c5f02272dfc0bd0fa24174
Cristian Rodriguez <chrodriguez34@gmail.com>
2022-12-14 15:00:41 -0500

    fixing bug in productNAmerHowTo

--- src/components/text/ProductNamer/ProductNamerHowTo.js
+++ src/components/text/ProductNamer/ProductNamerHowTo.js
@@ -16,14 +16,6 @@ export default function ProductNamerHowTo() {
                     <li>Product description: a non-profit that helps people find homeless people in need</li>
                     <li>Key Characteristics: kind, caring, humanitarian, charity</li>
                 </ul>
-                <ul>
-                    <li>Product description: a fashion website that finds stylists according to your fashion taste</li>
-                    <li>Key Characteristics: easy, modern, stylish, trendy, popular</li>
-                </ul>
-                <ul>
-                    <li>Product description: A bottle than automatically purifies any liquid inside of it</li>
-                    <li>Key Characteristics: clean, sleek, techy, hygenic, pure, healthy</li>
-                </ul>
             </Typography>
         </Box>
     )
